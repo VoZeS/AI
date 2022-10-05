251c2341875fa2d3b4e284084f9904efa88a26f6
VoZeS <davidboces12@hotmail.com>
2022-10-05 19:03:53 +0200

    CarsTrafficLights

--- AI-Project_GinuhGames/Assets/Scripts/TrafficLightsLogic.cs
+++ AI-Project_GinuhGames/Assets/Scripts/TrafficLightsLogic.cs
@@ -14,6 +14,11 @@ public class TrafficLightsLogic : MonoBehaviour
     public Light TL_Pedestrian_Green_L_Back;
     public Light TL_Pedestrian_Green_R_Back;
 
+    public Light TL_Car_Green_Front;
+    public Light TL_Car_Red_Front;
+    public Light TL_Car_Red_Back;
+    public Light TL_Car_Green_Back;
+
     public NavMeshAgent runner;
 
     private float timer;
@@ -33,6 +38,13 @@ public class TrafficLightsLogic : MonoBehaviour
         TL_Pedestrian_Red_R_Front.enabled = false;
         TL_Pedestrian_Red_R_Back.enabled = false;
 
+        // ---------------------------------------------- CarLights
+        // Enable Red Lights // Disable Green Lights
+        TL_Car_Green_Back.enabled = false;
+        TL_Car_Green_Front.enabled = false;
+        TL_Car_Red_Front.enabled = true;
+        TL_Car_Red_Back.enabled = true;
+
     }
 
     // Update is called once per frame
@@ -40,7 +52,7 @@ public class TrafficLightsLogic : MonoBehaviour
     {
         timer += Time.deltaTime;
 
-        // Green Lights Off // Red Lights On
+        // PEDESTRIAN: Green Lights Off // Red Lights On
         if(TL_Pedestrian_Green_L_Back.enabled == false &&
         TL_Pedestrian_Green_L_Front.enabled == false &&
         TL_Pedestrian_Green_R_Front.enabled == false &&
@@ -50,11 +62,17 @@ public class TrafficLightsLogic : MonoBehaviour
         TL_Pedestrian_Red_R_Back.enabled == true &&
         TL_Pedestrian_Red_R_Front.enabled == true)
         {
+            // CARS: Green Lights On // Reds Lights Off
+            TL_Car_Green_Back.enabled = true;
+            TL_Car_Green_Front.enabled = true;
+            TL_Car_Red_Back.enabled = false;
+            TL_Car_Red_Front.enabled = false;
+
             runner.areaMask = NavMesh.GetAreaFromName("PedestrianPass");
 
             if (timer >= 5)
             {
-                // Green Lights On // Red Lights Off
+                // PEDESTRIAN: Green Lights On // Red Lights Off
                 TL_Pedestrian_Green_L_Back.enabled = true;
                 TL_Pedestrian_Green_L_Front.enabled = true;
                 TL_Pedestrian_Green_R_Front.enabled = true;
@@ -65,17 +83,29 @@ public class TrafficLightsLogic : MonoBehaviour
                 TL_Pedestrian_Red_R_Back.enabled = false;
                 TL_Pedestrian_Red_R_Front.enabled = false;
 
+                // CARS: Green Lights Off // Reds Lights On
+                TL_Car_Green_Back.enabled = false;
+                TL_Car_Green_Front.enabled = false;
+                TL_Car_Red_Back.enabled = true;
+                TL_Car_Red_Front.enabled = true;
+
                 timer = 0;
             }
         }
-        else // Green Lights On // Red Lights Off
+        else // PEDESTRIAN: Green Lights On // Red Lights Off
         {
+            // CARS: Green Lights Off // Reds Lights On
+            TL_Car_Green_Back.enabled = false;
+            TL_Car_Green_Front.enabled = false;
+            TL_Car_Red_Back.enabled = true;
+            TL_Car_Red_Front.enabled = true;
+
             runner.areaMask = NavMesh.AllAreas;
 
             if (timer >= 5 &&
                 NavMesh.SamplePosition(runner.transform.position, out NavMeshHit hit, 1.0f, NavMesh.GetAreaFromName("PedestrianPass")))
             {
-                // Green Lights Off // Red Lights On
+                // PEDESTRIAN: Green Lights Off // Red Lights On
                 TL_Pedestrian_Green_L_Back.enabled = false;
                 TL_Pedestrian_Green_L_Front.enabled = false;
                 TL_Pedestrian_Green_R_Front.enabled = false;
@@ -85,6 +115,12 @@ public class TrafficLightsLogic : MonoBehaviour
                 TL_Pedestrian_Red_L_Front.enabled = true;
                 TL_Pedestrian_Red_R_Back.enabled = true;
                 TL_Pedestrian_Red_R_Front.enabled = true;
+
+                // CARS: Green Lights On // Reds Lights Off
+                TL_Car_Green_Back.enabled = true;
+                TL_Car_Green_Front.enabled = true;
+                TL_Car_Red_Back.enabled = false;
+                TL_Car_Red_Front.enabled = false;
 
                 timer = 0;
             }
