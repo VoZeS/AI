2ae2ea15d6eb325c20a6edcc40f9f2c9afec189b
Erik Martín Garzón <erikmarting1200@gmail.com>
2022-12-20 08:09:06 +0100

    pigs

---
+++ AI-Project_GinuhGames/Assets/Scripts/VillagerMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class VillagerMovement : MonoBehaviour
+{
+    NavMeshAgent agent;
+
+    public float radius = 10.0f;
+    public float offset = 10.0f;
+
+    private float watchTimer = 0.0f;
+    public bool wandering;
+    public bool watching;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        wandering = true;
+        watching = true;
+
+        agent.velocity = new Vector3(0.0f, 0.0f, 5.0f);
+
+    }
+
+    void Update()
+    {
+       Wander();
+    }
+
+    public void Wander()
+    {
+        watchTimer += Time.deltaTime;
+
+        if (wandering)
+        {
+            Vector3 localTarget = UnityEngine.Random.insideUnitCircle * radius;
+            localTarget += new Vector3(offset, 0, offset);
+
+            Vector3 worldTarget = transform.TransformPoint(localTarget);
+            worldTarget.y = 0f;
+
+            agent.destination = worldTarget;
+
+            wandering = false;
+            watching = true;
+
+        }
+
+        if (agent.velocity == new Vector3(0.0f, 0.0f, 0.0f) && !wandering)
+        {
+            if (watching)
+            {
+                watchTimer = 0.0f;
+                watching = false;
+
+             
+            }
+            else
+            {
+                wandering = true;
+            }
+        }
+        else
+        {
+            watching = true;
+        }
+    }
+}
